f3743b3cedf977d5b07add9c9effeb6c1f80b767
Andrew Cloud <cloud.andr@gmail.com>
2020-05-18 20:07:09 -0400

    added the ability to cycle through steps

--- javascript.js
+++ javascript.js
@@ -38,10 +38,52 @@ function nextSteps() {
 	next_step = next_step + 1;
 }
 
+// moves to previous steps
+function previousSteps() {
+	current_step = current_step - 1;
+	next_step = next_step - 1;
+}
+
+// checks to see if there's a previous step
+function isTherePreviousStep() {
+	if ((recipe[(current_step - 1)]) == undefined) {
+		return false;
+	}
+	else {
+		return true;
+	}	
+}
+
+// checks to see if there's a next step
+function isThereNextStep() {
+	if ((recipe[(next_step + 1)]) == undefined) {
+		return false;
+	}
+	else {
+		return true;
+	}	
+}
+
 // updates step text
 function updateStepsText(){
 	updateCurrentStep();
 	updateNextStep();
 }
 
+// sets the initial steps for the recipe
 updateStepsText();
+
+// right arrow keys moves steps forward and back
+window.addEventListener('keydown', function(e) {
+	console.log(e.keyCode);
+	if (e.keyCode == `39` && isThereNextStep()) {
+		nextSteps();
+		updateStepsText();
+		console.log("hi");
+	}
+	if (e.keyCode == `37` && isTherePreviousStep()) {
+		previousSteps();
+		updateStepsText();
+		console.log("hey");
+	}
+})
